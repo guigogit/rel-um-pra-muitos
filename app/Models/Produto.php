b028b89fe6fb6cdd04a76b9b441ee14d0e7bbb5a
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2023-09-22 16:06:23 -0300

    aula_121

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    use HasFactory;
+    function categoria() {
+        return $this->belongsTo('App\Models\Categoria');
+    }
 }
