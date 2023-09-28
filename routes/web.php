489436cf7a99a077277983fe6e302e3b4a27b58f
rodrigo <rodrigo.oliveira@spyonsky.com.br>
2023-09-28 16:20:38 -0300

    aula_126_concluída

--- routes/web.php
+++ routes/web.php
@@ -76,3 +76,49 @@ Route::get('/categoriasprodutos', function ()
         }
     }
 });
+
+
+Route::get('/categoriasprodutos/json', function (){
+    $cats = Categoria::with('produtos')->get();
+    return $cats->toJson();
+});
+
+Route::get('/adicionarproduto', function (){
+    $cat = Categoria::find(1);
+    $p = new Produto();
+    $p->nome = "Meu novo produto";
+    $p->estoque =10;
+    $p->preco = 100;
+    $p->categoria()->associate($cat);
+    return $p->toJson();
+
+
+});
+
+
+Route::get('/removerprodutocategoria', function (){
+    $p = Produto::find(1);
+    if(isset($p)){
+        $p->categoria()->dissociate();
+        $p->save();
+        return $p->toJson();
+    }
+
+
+
+
+});
+Route::get('/adicionarproduto/{catid}', function ($catid){
+    $cat = Categoria::with('produtos')->find($catid);
+    $p = new Produto();
+    $p->nome = "Meu novo produto adicionado 2";
+    $p->estoque =40;
+    $p->preco = 500;
+
+    if(isset($cat)){
+        $cat->produtos()->save($p);
+    }
+    $cat->load('produtos');
+    return $cat->toJson();
+
+});
